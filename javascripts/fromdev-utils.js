87e188c7f8fa9d898408e3c33b3d66da8f987e2c
Sachin Joshi <software.wikipedia@gmail.com>
2017-01-08 10:24:53 -0800

    Add files via upload

---
+++ javascripts/fromdev-utils.js
@@ -0,0 +1,17 @@
+var Utils = Utils || {};
+Utils.amazon = {
+    onlyASINRegex: new RegExp("([a-zA-Z0-9]{10})"),
+
+    extractASIN: function(url) {
+        var regex = new RegExp("/([a-zA-Z0-9]{10})(?:[/?]|$)");
+        var m = url.match(regex);
+        if (m) {
+            return m[0].replace(new RegExp('\/', 'g'), '');
+        }
+        return '';
+    },
+    isValidASIN = function(input) {
+        return onlyASINRegex.test(input);
+    }
+
+};
