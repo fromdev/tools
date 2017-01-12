fc7f3b16735472eb5610cd69d51429269a183405
Sachin Joshi <software.wikipedia@gmail.com>
2017-01-11 22:28:19 -0800

    Delete arrayUtil.js

--- javascripts/arrayUtil.js
+++
@@ -1,7 +0,0 @@
-var removeDuplicates = function(arr) {
-    var uniques = [];
-    $.each(arr, function(i, el) {
-        if ($.inArray(el, uniques) === -1) uniques.push(el);
-    });
-    return uniques;
-};
