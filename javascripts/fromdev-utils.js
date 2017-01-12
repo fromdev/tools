e42be1b2dce2a441fd814e23fc829434c36d69d8
Sachin Joshi <software.wikipedia@gmail.com>
2017-01-11 18:26:53 -0800

    Add files via upload

--- javascripts/fromdev-utils.js
+++ javascripts/fromdev-utils.js
@@ -110,7 +110,7 @@ Utils.urlutil = {
 
 Utils.randomutil = {
     getRandomInt : function(min, max) {
-        min = min || 1;
+        min = min || 0;
         max = max || 10;
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
