1a79f7eaffa6456f8b9aa159c06c90d361bc152b
Sachin Joshi <software.wikipedia@gmail.com>
2017-11-26 13:07:59 -0800

    Add files via upload

--- math/javascript/MultiplicationProblem.js
+++ math/javascript/MultiplicationProblem.js
@@ -4,6 +4,13 @@ class MultiplicationProblem {
     this.second = second;
   }
 
+  get display() {
+    return this.displayProblem() + " = " + this.answer();
+  }
+  get displayProblem() {
+    return this.first + " x " + this.second;
+  }
+
   get answer() {
     return this.first * this.second;
   }
