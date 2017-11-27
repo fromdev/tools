6517312c59607d586b97f9ea3ce664a4c7a2d220
Sachin Joshi <software.wikipedia@gmail.com>
2017-11-27 11:49:37 -0800

    Add files via upload

--- math/javascript/MultiplicationProblem.js
+++ math/javascript/MultiplicationProblem.js
@@ -26,8 +26,9 @@ class MultiplicationProblem {
     choices.add(answ + 1);
     choices.add(answ + RandomUtils.getRandomInt(1,10));
     //Ensure we have 6 choices
+    var choicesRange = (answ > 100) ? answ : 100;
     while(choices.size < 6) {
-      choices.add(RandomUtils.getRandomInt(1,answ))
+      choices.add(RandomUtils.getRandomInt(1,choicesRange));
     }
     return Array.from(choices);
   }
