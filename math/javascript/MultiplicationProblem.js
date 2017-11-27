eabb10c8cdc203ec6519a2d59f163966b5e0762c
Sachin Joshi <software.wikipedia@gmail.com>
2017-11-26 18:19:12 -0800

    Add files via upload

--- math/javascript/MultiplicationProblem.js
+++ math/javascript/MultiplicationProblem.js
@@ -34,7 +34,7 @@ class MultiplicationProblem {
   }
   static generateProblems(range) {
     var problems = new Array();
-    if(range && range.start && range.end) {
+    if(range && range.start >= 0 && range.end >=0) {
       for(i = range.start; i<range.end+1; i++) {
         for(j = range.start; j<range.end+1; j++) {
             problems.push(new MultiplicationProblem(i,j));
