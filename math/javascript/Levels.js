e4fc01c9d8691fa7fdd6c7ac77f4a8395ce3838e
Sachin Joshi <software.wikipedia@gmail.com>
2017-11-27 18:13:34 -0800

    Add files via upload

--- math/javascript/Levels.js
+++ math/javascript/Levels.js
@@ -3,11 +3,13 @@ Levels.TWO = {
   "id" : 2,
   "nextLevel":3,
   "choicesGiven":6,
-  "range" : {"start":5,"end":9}
+  "range" : {"start":5,"end":9},
+  "points" : 7
 };
 Levels.ONE = {
   "id" : 1,
   "nextLevel":Levels.TWO,
   "choicesGiven":6,
-  "range" : {"start":0,"end":4}
+  "range" : {"start":0,"end":4},
+  "points" : 5
 };
