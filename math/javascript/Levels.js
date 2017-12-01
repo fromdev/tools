72811953927b2b2d72e908f27480f0ebc4569820
Sachin Joshi <software.wikipedia@gmail.com>
2017-11-30 22:29:31 -0800

    Add files via upload

--- math/javascript/Levels.js
+++ math/javascript/Levels.js
@@ -1,42 +1,18 @@
 var Levels = Levels || {};
-Levels.THREE = {
-  "id" : 3,
-  "nextLevel":3,
-  "choicesGiven":6,
-  "range" : {"start":5,"end":9},
-  "points" : 7
-};
-Levels.TWO = {
-  "id" : 2,
-  "nextLevel":Levels.THREE,
-  "choicesGiven":6,
-  "range" : {"start":5,"end":9},
-  "points" : 7
+
+var calculateNextLevel = function(prevLevel) {
+  return {
+    "id" : prevLevel.id + 1,
+    "nextLevel":calculateNextLevel,
+    "choicesGiven":6,
+    "range" : {"start":prevLevel.range.end,"end":prevLevel.range.end + 4},
+    "points" : prevLevel.points + 1
+  };
 };
 Levels.ONE = {
   "id" : 1,
-  "nextLevel":Levels.TWO,
+  "nextLevel":calculateNextLevel,
   "choicesGiven":6,
   "range" : {"start":0,"end":4},
   "points" : 5
 };
-//1 = 0 - 4
-//2 = 4 - 6
-//3 = 7 - 9
-Levels.ALL = [];
-var prevLevel;
-var start = 0;
-var end = 0;
-for(int i = 26; i > 1; i++) {
-  end = start + 4;
-  var tmpLevel = {
-    "id" : i,
-    "nextLevel": prevLevel,
-    "choicesGiven":6,
-    "range" : {"start":start-2,"end":end},
-    "points" : 3 * i
-  };
-  Levels.ALL.push(tmpLevel);
-  prevLevel = tmpLevel;
-  start = end;
-}
