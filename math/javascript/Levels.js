1316bcaeeaa0fdb3aef992cf693e01d0ee7752ee
Sachin Joshi <software.wikipedia@gmail.com>
2017-11-30 22:53:55 -0800

    Add files via upload

--- math/javascript/Levels.js
+++ math/javascript/Levels.js
@@ -1,18 +1,17 @@
 var Levels = Levels || {};
 
-var calculateNextLevel = function(prevLevel) {
-  return {
-    "id" : prevLevel.id + 1,
-    "nextLevel": function(level) { return calculateNextLevel(level);},
-    "choicesGiven":6,
-    "range" : {"start":prevLevel.range.end,"end":prevLevel.range.end + 4},
-    "points" : prevLevel.points + 1
-  };
-};
 Levels.ONE = {
   "id" : 1,
-  "nextLevel": function(level) { return calculateNextLevel(level);},
   "choicesGiven":6,
   "range" : {"start":0,"end":4},
   "points" : 5
+};
+
+Levels.NextLevel = function(prevLevel) {
+  return (prevLevel) ? {
+    "id" : prevLevel.id + 1,
+    "choicesGiven":6,
+    "range" : {"start":prevLevel.range.end,"end":prevLevel.range.end + 4},
+    "points" : prevLevel.points + 1
+  } : Levels.ONE;
 };
