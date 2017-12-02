f9cf16b152991021e51a5d7fa3c6dc8b1d659444
Sachin Joshi <software.wikipedia@gmail.com>
2017-12-01 22:41:09 -0800

    Add files via upload

--- math/javascript/Levels.js
+++ math/javascript/Levels.js
@@ -15,3 +15,49 @@ Levels.NextLevel = function(prevLevel) {
     "points" : prevLevel.points + 1
   } : Levels.ONE;
 };
+
+Levels.CurrentLevel = {
+  Instance : function() {
+    return StorageUtils.getJSON("CURRENT_LEVEL");
+  },
+  initialize : function(level) {
+    //default start with level one
+    if(!level || !level.id) {
+      level = Levels.ONE;
+    }
+    if(level) {
+      if(!level.problems) {
+        level.problems = ArrayUtils.shuffle(MultiplicationProblem.generateProblems(level.range));
+      }
+      var lvlStr = JSON.stringify(level);
+      StorageUtils.setItem("CURRENT_LEVEL",lvlStr);
+      $("#levelHeading").text(level.id);
+      $(".answerChoice").show();
+      $("#nextLevel").hide();
+      showPoints();
+      console.log('Level initialized ' + lvlStr);
+    }
+  },
+  findNextProblem : function() {
+     var level = CurrentLevel.Instance();
+     if(level.problems.length > 0) {
+       var problem = level.problems.last();
+       return new MultiplicationProblem(problem.first, problem.second);
+     }
+  },
+  moveProblemToEnd : function() {
+    var level = CurrentLevel.Instance();
+    if(level.problems.length > 0) {
+      var problem = level.problems.pop();
+      level.problems.unshift(problem);
+      StorageUtils.setItem("CURRENT_LEVEL",JSON.stringify(level));
+    }
+  },
+  removeLastProblem : function(problem) {
+    var level = CurrentLevel.Instance();
+    if(level.problems.length > 0) {
+      level.problems.pop();
+      StorageUtils.setItem("CURRENT_LEVEL",JSON.stringify(level));
+    }
+  }
+};
