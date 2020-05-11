7bbfbc97aedf5ed678968705767d1affd4587175
Sachin, Joshi(sacjoshi) <sacjoshi@ebay.com>
2020-05-11 12:17:23 -0700

    Property to JSON
    Property to JSON

---
+++ javascripts/property2json.js
@@ -0,0 +1,25 @@
+const property2json = function(propkeyarr, value, json) {
+    if(!json) json = {};
+    if(!propkeyarr) return;
+    if(!value) return;
+    let parent = json;
+    for(let i = 0; i < propkeyarr.length; i++ ) {
+        let name = cleanse(propkeyarr[i]);
+        if(i === propkeyarr.length-1) {
+            parent[name] = value;
+        } else {
+            parent[name] = parent[name] || {};
+            parent = parent[name];
+        }
+    }
+    return json;
+};
+
+const cleanse = function(name) {
+    try {
+        return name.split(' ').join('');
+    } catch(e) {
+        return name;
+    }
+   
+}
