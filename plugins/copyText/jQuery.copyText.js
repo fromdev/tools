aafd34ab36904c010b157c6692e5d60ae53d3a7e
Sachin Joshi <sacjoshi@ebay.com>
2021-07-15 12:07:09 -0700

    copy text plugin for jquery
    copy text plugin for jquery

---
+++ plugins/copyText/jQuery.copyText.js
@@ -0,0 +1,18 @@
+! function(n) {
+  const copyTextAction = (id) => {
+    if (!id) return false;
+    const copyText = document.getElementById(id);
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    return true;
+  };
+  n.fn.copyText = function(context) {
+    if(!context || !context.textElementId) return; //nothing to register
+    var u = n(this);
+      n.click(function() {
+      const val = copyTextAction(context.textElementId);
+      if(val && context.confirm) context.confirm();
+    });
+  }
+}(jQuery);
