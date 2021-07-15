141370bd0fb7cb1cc8e9e2cd0be360ee21b7defe
Sachin Joshi <sacjoshi@ebay.com>
2021-07-15 14:15:22 -0700

    ux utils added
    ux utils added

---
+++ javascripts/fromdev-ux-addon.js
@@ -0,0 +1,23 @@
+var UxUtils = UxUtils || {};
+
+UxUtils.confirmShowHide = (id) => {
+  $(id).show().delay(5000).fadeOut();
+};
+
+UxUtils.copyTextAddon = {
+  register: (id) => {
+    if(!id) return;
+    const buttonId = `#copy-${id}`
+    const copyButton = $(buttonId);
+    if(!copyButton) return;
+    const confirmSpanId = `#confirm-${id}`;
+    const confirmSpan = $(confirmSpanId);
+    if(!confirmSpan) return;
+    copyButton.copyText({
+      textElementId: id,
+      confirm: function() {
+        UxUtils.confirmShowHide(confirmSpanId);
+      }
+    });
+  }
+};
