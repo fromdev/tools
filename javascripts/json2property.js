41907c7a7f42220fc938ef0ff3e73518e9f42d66
Sachin Joshi <sacjoshi@ebay.com>
2021-07-19 11:11:39 -0700

    added separator and enclose support
    added separator and enclose support

--- javascripts/json2property.js
+++ javascripts/json2property.js
@@ -4,38 +4,44 @@ var isNumeric = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-var formatJsonPropMappings = function(jsonparent, k, v) {
-    var jsonpath = formatJsonPath(jsonparent,k,v);
+var formatJsonPropMappings = function(jsonparent, k, v, context) {
+    var jsonpath = formatJsonPath(jsonparent,k,v, context);
     var mapping = {"json":jsonpath,"value":v};
     const vtype = typeof v;
     if(vtype === "string" || vtype === 'number'){
-      mappings.push(jsonpath + "=" + v);
-    }    
+      if(context.enclose) {
+        path = `${enclose}${jsonpath}${enclose}=${enclose=${v}${enclose}`;
+      } else {
+        mappings.push(`${jsonpath}=${v}`);
+      }
+    }
     return mapping;
 };
 
 
-var formatJsonPath = function (parent, k, v) {
+var formatJsonPath = function (parent, k, v, context) {
     var path = '';
+    const separator = ((context && context.separator) ? context.separator : '.') || '.';
     if (parent) {
         if (isNumeric(k)) {
             path = parent + '[' + k + ']';
         } else {
-            path = parent + '.' + k;
+            path = parent + separator + k;
         }
     } else {
         path = k;
     }
+
     return path;
 };
 
-var traverseBoth = function(jsonObj,parentJson, callback) {
+var traverseBoth = function(jsonObj,parentJson, callback, context) {
     if( typeof jsonObj == "object" ) {
         $.each(jsonObj, function(k,v) {
             // k is either an array index or object key
-            var mapping = callback(parentJson, k,v);
+            var mapping = callback(parentJson, k,v, context);
             // console.log(JSON.stringify(mapping));
-            traverseBoth(v,mapping.json,callback);
+            traverseBoth(v,mapping.json,callback, context);
         });
     }
     else {
