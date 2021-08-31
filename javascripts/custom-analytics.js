f77e15e50a2916e72ff9556f51a96856de1ae0d4
Sachin Joshi <software.wikipedia@gmail.com>
2021-08-31 14:45:30 -0700

    collect urls

---
+++ javascripts/custom-analytics.js
@@ -0,0 +1,12 @@
+const CustomAnalytics = CustomAnalytics || {};
+
+CustomAnalytics.collector = {
+    collect: (context) => {
+        if(!context || !context.eventAction || !context.eventName || !context.message) return;
+        try {
+            ga('send', 'event', context.eventName, context.eventAction, context.message);
+        } catch(e) {
+            //ignore silently
+        }
+      }
+}
