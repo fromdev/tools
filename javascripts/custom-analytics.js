4021b04edf1c8e0ab9c5de52381c93277500b0fd
Sachin Joshi <software.wikipedia@gmail.com>
2021-09-07 11:12:05 -0700

    error handing collection uniform

--- javascripts/custom-analytics.js
+++ javascripts/custom-analytics.js
@@ -3,10 +3,28 @@ var CustomAnalytics = CustomAnalytics || {};
 CustomAnalytics.collector = {
     collect: (context) => {
         if(!context || !context.eventAction || !context.eventName || !context.message) return;
+        
         try {
             ga('send', 'event', context.eventName, context.eventAction, context.message);
         } catch(e) {
             //ignore silently
         }
+      },
+      collectError: (context, err) => {
+        context.errorContext = (Page || { request: {}}).request || {};
+        context.errorContext.url = window.location.href;
+        context.errorContext.userAgent = navigator.userAgent;
+        try {
+            context.eventName = `$err`;
+        } catch(e) {
+            context.eventName = `Unknown Error`;
+        }
+        CustomAnalytics.collector.collect(
+          {
+            eventName: context.eventName,
+            eventAction: context.eventAction,
+            message: `${JSON.stringify(context.errorContext)}`
+          }
+        );
       }
 };
