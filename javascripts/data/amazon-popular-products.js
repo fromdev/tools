95a5610307c380aabe5928264ae9042e8b96cf4d
Sachin Joshi <sacjoshi@ebay.com>
2024-11-23 12:50:27 -0800

    black friday deals update

--- javascripts/data/amazon-popular-products.js
+++ javascripts/data/amazon-popular-products.js
@@ -5,7 +5,7 @@
         const rows = [];
         rows.push(context.createTableHeader());
         const cleanList = AmazonPopularProducts.list.filter(e => e && e.asin && e.name);
-        const someRandomProducts = (ArrayUtils) 
+        const someRandomProducts = (ArrayUtils)
             ? ArrayUtils.someRandomItems(cleanList, AmazonPopularProducts.LIMIT)
             : [];
         someRandomProducts.forEach(r => rows.push(context.createRow(r)));
@@ -25,14 +25,6 @@
         {
             name: 'Apple iPhone SE (2nd Generation), 64GB, Black - Unlocked (Renewed Premium)',
             asin: 'B08R98BFZS'
-        },
-        {
-            name: 'SAMSUNG 65-inch Class Q80A Series ? QLED 4K UHD Smart TV with Alexa Built-in',
-            asin: 'B093KSGPK3'
-        },
-        {
-            name: '2020 Apple MacBook Pro with Intel Processor (13-inch, 16GB RAM, 1TB SSD Storage) - Silver',
-            asin: 'B08821611Y'
         },
         {
             name: 'SENNHEISER Momentum 3 Wireless Noise Cancelling Headphones with Alexa, Auto On/Off, Smart Pause Functionality and Smart Control App, Black',
@@ -55,20 +47,12 @@
             asin: 'B07H4YQF3Q'
         },
         {
-            name: '$10 PlayStation Store Gift Card [Digital Code]',
-            asin: 'B00GAC1D2G'
-        },
-        {
             name: 'Crocs Mens and Womens Classic Clog',
             asin: 'B08KHCJJMF'
         },
         {
             name: 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
             asin: '0735211299'
-        },
-        {
-            name: 'GE Profile OPAL01GEPKT Opal | Countertop Nugget Ice Maker, Stainless Steel Wrap with Gray Accents & LED Lighting',
-            asin: 'B07YF9SGBW'
         },
         {
             name: 'Paper Mate Flair Felt Tip Pens',
@@ -157,6 +141,86 @@
         {
             name: 'Listerine Total Care Anticavity Mouthwash, 6 Benefit Fluoride Mouthwash for Bad Breath and Enamel Strength, Fresh Mint Flavor, 1 L',
             asin: 'B00495Q5OW'
+        },
+        {
+          name: "DEWALT 20V MAX Cordless Drill and Impact Driver, Power Tool Combo Kit with 2 Batteries and Charger (DCK240C2)",
+          asin: "B00IJ0ALYS"
+        },
+        {
+          name: "Breville the Smart Oven® Air Fryer Pro, Convection Countertop Oven, Air Fryer Toaster Oven Combo, BOV900BSS, Brushed Stainless Steel",
+          asin: "B01N5UPTZS"
+        },
+        {
+          name: "SanDisk 256GB High Endurance Video microSDXC Card with Adapter for Dash Cam and Home Monitoring systems - C10, U3, V30, 4K UHD, Micro SD Card - SDSQQNR-256G-GN6IA",
+          asin: "B07P4HBRMV"
+        },
+        {
+          name: "Philips Sonicare ProtectiveClean 5300 Rechargeable Electric Toothbrush, with Pressure Sensor, 3 Cleaning Modes, SmarTimer and QuadPacer, with 2 Bonus Brush Heads, Travel Case, Black, Model HX6423/34",
+          asin: "B084TM4XKG"
+        },
+        {
+          name: "Apple AirPods Max Wireless Over-Ear Headphones, Active Noise Cancelling, Transparency Mode, Personalized Spatial Audio, Dolby Atmos, Bluetooth Headphones for iPhone – Space Gray",
+          asin: "B08PZHYWJS"
+        },
+        {
+          name: "AMD Ryzen 7 5700X 8-Core, 16-Thread Unlocked Desktop Processor",
+          asin: "B09VCHQHZ6"
+        },
+        {
+          name: "WD 20TB Elements Desktop External Hard Drive, USB 3.0 drive for plug-and-play storage - WDBWLG0200HBK-NESN",
+          asin: "B09VCXWPQG"
+        },
+        {
+          name: "Sony WH-1000XM5 The Best Wireless Noise Canceling Headphones, Made Of Soft Fit Synthetic Leather, Integrated Processor V1, With 4 Beamforming Microphones, Up To 30-Hour Battery Life, Black",
+          asin: "B09XS7JWHH"
+        },
+        {
+          name: "Apple iPad (10th Generation): with A14 Bionic chip, 10.9-inch Liquid Retina Display, 64GB, Wi-Fi 6, 12MP front/12MP Back Camera, Touch ID, All-Day Battery Life – Silver",
+          asin: "B0BJLCWFNM"
+        },
+        {
+          name: "Bose QuietComfort Ultra Bluetooth Headphones, Wireless Headphones with Spatial Audio, Over Ear Noise Cancelling Headphones with Mic, Up To 24 Hours of Battery Life, Black",
+          asin: "B0CCZ1L489"
+        },
+        {
+          name: "Bose QuietComfort Bluetooth Headphones, Wireless Headphones, Over Ear Noise Cancelling Headphones with Mic, Up To 24 Hours of Battery Life, Black",
+          asin: "B0CCZ26B5V"
+        },
+        {
+          name: "PlayStation®5 console (slim)",
+          asin: "B0CL61F39H"
+        },
+        {
+          name: "Beats Solo 4 - Wireless Bluetooth On-Ear Headphones, Apple & Android Compatible, Up to 50 Hours of Battery Life - Matte Black",
+          asin: "B0CZPLV566"
+        },
+        {
+          name: "MSI GeForce RTX 4070 Ti Super 16G Ventus 3X Black OC Graphics Card (NVIDIA RTX 4070 Ti Super, 256-Bit, Extreme Clock: 2655 MHz, 16GB GDRR6X 21Gbps, HDMI/DP, Ada Lovelace Architecture)",
+          asin: "B0D6ZN2P1J"
+        },
+        {
+          name: "Apple AirTag 4 Pack",
+          asin: "B0D54JZTHY"
+        },
+        {
+          name: "Apple Watch Series 10 [GPS 46mm case] Smartwatch with Jet Black Aluminium Case with Black Sport Band - M/L. Fitness Tracker, ECG App, Always-On Retina Display, Water Resistant",
+          asin: "B0DGHQ2QH6"
+        },
+        {
+          name: "All-new Amazon Kindle Paperwhite Signature Edition (32 GB) – Our fastest Kindle with auto-adjusting front light, wireless charging, and weeks of battery life – Metallic Black",
+          asin: "B0C8RR4WN3"
+        },
+        {
+          name: "New Amazon Kindle (16 GB) - Lightest and most compact Kindle, with glare-free display, faster page turns, adjustable front light, and long battery life - Black",
+          asin: "B0CNV9F72P"
+        },
+        {
+          name: "All-new Amazon Kindle Paperwhite (16 GB) – Our fastest Kindle ever, with new 7\" glare-free display and weeks of battery life – Black",
+          asin: "B0CFPJYX7P"
+        },
+        {
+          name: "Apple 2024 MacBook Air 13-inch Laptop with M3 chip: Built for Apple Intelligence, 13.6-inch Liquid Retina Display, 16GB Unified Memory, 256GB SSD Storage, Backlit Keyboard, Touch ID; Midnight",
+          asin: "B0D7ZX2JNV"
         },
         {
             name: '',
